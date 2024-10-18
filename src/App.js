6d36d120b0f74d1f83cdde09cef9c5fea42e125a
jwsong <jwsong@sjinc.co.kr>
2024-10-18 09:55:24 +0900

    set header, main, footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import Home from './pages/Home'
 import Today from './pages/Today'
@@ -14,9 +14,15 @@ import Video from './pages/Video'
 import Search from './pages/Search'
 import Not from './pages/Not'
 
+import Header from './components/section/Header'
+import Main from './components/section/Main'
+import Footer from './components/section/Footer'
+
 const App = () => {
   return (
       <BrowserRouter>
+        <Header />
+        <Main>
           <Routes>
               <Route path='/' element={<Home />} />
               <Route path='/today' element={<Today />} />
@@ -31,6 +37,8 @@ const App = () => {
               <Route path='/search/:searchId' element={<Search />} />
               <Route path='*' element={<Not />} />
           </Routes>
+        </Main>
+        <Footer />
       </BrowserRouter>
   );
 }
